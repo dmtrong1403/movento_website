96f82811b86501617f5bef3d28a49ae85984c726
Tony <trong.dm.it@gmail.com>
2018-11-18 01:37:44 +0700

    Setting: allow web domain 'movento.com.vn'

--- movento_root/movento_root/settings.py
+++ movento_root/movento_root/settings.py
@@ -23,11 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yk&+c0dt$xuml4)t6d+!t_1pxpe!9*^m5hz4is8akcw2+l)we!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '103.104.117.174',
     '127.0.0.1',
+    'movento.com.vn',
 ]
 
 
